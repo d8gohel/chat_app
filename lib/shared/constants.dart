d1713a915c49c1eee4263048d3348c6410c1ac53
D8GOHEL\d8gohel <d8gohel>
2024-07-30 21:28:02 +0530

    code push

---
+++ lib/shared/constants.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+
+class Constants {
+static String apiKey= "AIzaSyAcEdVEHFiXypuHQ1merFAtL9cawxSIRDQ";
+
+
+  static String projectId= "chat-bca85";
+  static String messagingSenderId= "672417546831";
+  static String appId= "1:672417546831:web:1a1590ba6916b553ada03a";
+  static String measurementId= "G-4J6THBQ9NP";
+  Color primaryColor =Colors.limeAccent;
+}
